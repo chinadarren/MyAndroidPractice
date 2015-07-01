00befbc3bcff9983e9176a65724951c88e933d9f
LeiChen <chinadarren@outlook.com>
2015-07-01 11:59:40 +0800

    Firm

--- src/com/example/MyAndroidPractice/MyActivity.java
+++ src/com/example/MyAndroidPractice/MyActivity.java
@@ -13,7 +13,57 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    public void onOclick(View v){
 
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnGridLayout:
+                setContentView(R.layout.gird_layout);
+                break;
+            case R.id.btnReturn:
+                setContentView(R.layout.main);
+                break;
+            case R.id.btnLinearLayout:
+                setContentView(R.layout.linear_layout);
+                break;
+            case R.id.linearReturn:
+                setContentView(R.layout.main);
+                break;
+            case R.id.btnTableLayout:
+                setContentView(R.layout.table_layout);
+                break;
+            case R.id.tableReturn:
+                setContentView(R.layout.main);
+                break;
+            case R.id.btnRelative:
+                setContentView(R.layout.reative_layout);
+                break;
+            case R.id.reativeReturn:
+                setContentView(R.layout.main);
+                break;
+            case R.id.linearBrief:
+                setContentView(R.layout.linear_brief);
+                break;
+            case R.id.linearBriefreturn:
+                setContentView(R.layout.linear_layout);
+                break;
+            case R.id.btnLinearBriefreturn:
+                setContentView(R.layout.main);
+                break;
+            case R.id.reativeBrief:
+                setContentView(R.layout.reative_brief);
+                break;
+            case R.id.reativeBriefReturn:
+                setContentView(R.layout.reative_layout);
+                break;
+            case R.id.btnRelativeReturn:
+                setContentView(R.layout.main);
+                break;
+//            case R.id.tableBriefReturn:
+//                setContentView(R.layout.table_layout);
+//                break;
+//            case R.id.btnTableBriefReturn:
+//                setContentView(R.layout.main);
+//                break;
+        }
     }
 }
